19a6b735d6f231d4261d8def99dccfc4326344eb
kevinnail <fumalicious@gmail.com>
2022-09-13 16:13:07 -0700

    state related variables defined + touch up on id's/ classes in html

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 
 /* State */
 
+const cPlayImgDiv = document.getElementById('cPlay-img-div'); //for showing/ hiding cplay-img div
+const cPlayImg = document.getElementById('computer-choiceImg'); //for selecting R/P/S img for computer
+
+const playerRockImg = document.getElementById('player-rockImg'); //player rock img
+const playerPaperImg = document.getElementById('player-paper-img'); // player paper img
+const playerScissorsImg = document.getElementById('player-scissors-img'); // player scissors img
+
+const rockBtn = document.getElementById('rockBtn'); //rock button
+const paperBtn = document.getElementById('paperBtn'); //paper button
+const scissorsBtn = document.getElementById('scissorsBtn'); //scissors button
+const playerBtns = document.getElementById('player-buttons'); //for showing/ hiding all player buttons
+
+const playAgainBtn = document.getElementById('play-again-btn'); // play again button
+
 /* Actions */
 function loadPage() {}
 
@@ -15,6 +29,6 @@ function loadPage() {}
 /* Run page load code */
 loadPage();
 
-let cPlay = 'paper';
-const computerChoice = document.getElementById('computer-choice');
-computerChoice.src = 'assets/' + cPlay + '.png';
+// let cPlay = 'scissors';
+// const computerChoice = document.getElementById('computer-choice');
+// computerChoice.src = 'assets/' + cPlay + '.png';
