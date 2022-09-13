c3c254765397409cf3eaf8e82e65e12e78685c99
kevinnail <fumalicious@gmail.com>
2022-09-13 15:42:52 -0700

    complete html and css

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ function loadPage() {}
 
 /* Run page load code */
 loadPage();
+
+let cPlay = 'paper';
+const computerChoice = document.getElementById('computer-choice');
+computerChoice.src = 'assets/' + cPlay + '.png';
