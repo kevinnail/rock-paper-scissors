8bb981a24e11e7ea4cd9502ea5f63a6cffefd165
kevinnail <fumalicious@gmail.com>
2022-09-13 17:04:26 -0700

    add event listeners to each player button (rock/ paper/ scissors choice)

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ let gameState = 'guess';
 
 const cPlayImgDiv = document.getElementById('cPlay-img-div'); //for showing/ hiding cplay-img div
 const cPlayImg = document.getElementById('computer-choiceImg'); //for selecting R/P/S img for computer
+const gameComment = document.getElementById('game-comment');
 
-const playerRockImg = document.getElementById('player-rockImg'); //player rock img
+const playerRockImg = document.getElementById('player-rock-img'); //player rock img
 const playerPaperImg = document.getElementById('player-paper-img'); // player paper img
 const playerScissorsImg = document.getElementById('player-scissors-img'); // player scissors img
 
@@ -18,27 +19,50 @@ const playerBtns = document.getElementById('player-buttons'); //for showing/ hid
 
 const playAgainBtn = document.getElementById('play-again-btn'); // play again button
 
+let guess = ''; // for holding player's choice
+
 /* Actions */
-function loadPage() {}
+function loadPage() {
+    restartGame();
+}
+
+function makePlay(playerChoice) {
+    gameState = 'results';
+    guess = playerChoice;
+    console.log('hi');
+}
+// }
 
 /* Components */
 
 /* Component */
+
 // get DOM
 // display
 // event listeners
 
+rockBtn.addEventListener('click', () => {
+    makePlay('rock');
+});
+paperBtn.addEventListener('click', () => {
+    makePlay('paper');
+});
+scissorsBtn.addEventListener('click', () => {
+    makePlay('scissors');
+});
+
 /* Run page load code */
 function restartGame() {
+    gameState = 'guess';
     cPlayImgDiv.classList.add('hidden');
     playAgainBtn.classList.add('hidden');
     playerRockImg.classList.remove('hidden');
     playerPaperImg.classList.remove('hidden');
     playerScissorsImg.classList.remove('hidden');
+    gameComment.textContent = "Let's play!";
 }
 
 loadPage();
-restartGame();
 // let cPlay = 'scissors';
 // const computerChoice = document.getElementById('computer-choice');
 // computerChoice.src = 'assets/' + cPlay + '.png';
